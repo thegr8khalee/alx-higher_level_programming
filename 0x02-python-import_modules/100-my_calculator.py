1997c46349acc26b66190d944cfc02281bd0ff6f
thegr8khalee <thegr8khalee@gmail.com>
2023-10-05 12:15:55 -0700

    mm

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from calculator_1 import add , sub, mul, div
+    from calculator_1 import add, sub, mul, div
     import sys
 
     if len(sys.argv) - 1 != 3:
@@ -15,5 +15,4 @@ if __name__ == "__main__":
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
-
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
