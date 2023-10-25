ee4ab9f5c27fb235daf0bd1183f55d9844abb654
thegr8khalee <kaliibro777@gmail.com>
2023-10-25 11:32:01 +0300

    null

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-class Square:
-    pass
+"""Square class to represent a square"""
 
-if __name__ == "__main__":
-    my_square = Square()
-    print(type(my_square))
-    print(my_square.__dict__)
+
+class Square():
+    """Empty class"""
+    pass
