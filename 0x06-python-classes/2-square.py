0917809d8e13b88e0dd06e405d97c11ec1df1d1f
thegr8khalee <kaliibro777@gmail.com>
2023-10-25 11:36:37 +0300

    null

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+
+"""..."""
+
+
 class Square:
+    """..."""
+
     def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
