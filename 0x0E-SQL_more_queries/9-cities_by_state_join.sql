e7c4545232659846a8cc911c44f71a67783ecce1
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-02-15 01:42:43 +0300

    9

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- lists all cities contained in the database hbtn_0d_usa
+SELECT cities.id, cities.name AS city_name, states.name AS state_name
+FROM hbtn_0d_usa.cities
+JOIN hbtn_0d_usa.states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
