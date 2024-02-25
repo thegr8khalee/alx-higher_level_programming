61c58ed826596c5446d56be001c60c629dac3953
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-02-25 21:29:15 +0300

    mm

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -49,3 +49,14 @@ class Base:
             else:
                 list_dicts = [o.to_dictionary() for o in list_objs]
                 jsonfile.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """_summary_
+
+        Args:
+            json_string (_type_): _description_
+        """
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
