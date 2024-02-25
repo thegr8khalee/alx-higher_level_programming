fa4ca5baf148985d5d3888ced2a53ba3c8057342
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-02-25 16:00:28 +0300

    mm

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,6 +9,7 @@ class Rectangle(Base):
     """
     documented
     """
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """
         documented
@@ -18,13 +19,55 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+
     @property
     def width(self):
-        """_summary_
-        """
+        """_summary_"""
         return self.__width
+
     @width.setter
     def width(self, value):
-        """_summary_
-        """
+        """_summary_"""
         self.__width = value
+
+    @property
+    def height(self):
+        """_summary_"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """_summary_
+
+        Args:
+            value (_type_): _description_
+        """
+        self.__height = value
+
+    @property
+    def x(self):
+        """_summary_"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """_summary_
+
+        Args:
+            value (_type_): _description_
+        """
+        self.__x = value
+
+    @property
+    def y(self):
+        """_summary_"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """_summary_
+
+        Args:
+            value (_type_): _description_
+        """
+        self.__y = value
