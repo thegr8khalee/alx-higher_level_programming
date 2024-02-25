ed7524b853abbf1578e003315b677344cf9c6d21
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-02-25 15:39:23 +0300

    mm

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+from models.base import Base
+
+"""_summary_
+"""
+
+
+class Rectangle(Base):
+    """_summary_
+
+    Args:
+        Base (_type_): _description_
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """_summary_
+
+        Args:
+            width (_type_): _description_
+            height (_type_): _description_
+            x (int, optional): _description_. Defaults to 0.
+            y (int, optional): _description_. Defaults to 0.
+            id (_type_, optional): _description_. Defaults to None.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
