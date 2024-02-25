d9ea49a23617a21a979545469be4d34c5efa9939
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-02-25 15:45:55 +0300

    mm

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from models.base import Base
+
 """
 documented
 """
@@ -8,21 +9,11 @@ documented
 class Rectangle(Base):
     """
     documented
-
-    Args:
-        Base (_type_): _description_
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """
         documented
-
-        Args:
-            width (_type_): _description_
-            height (_type_): _description_
-            x (int, optional): _description_. Defaults to 0.
-            y (int, optional): _description_. Defaults to 0.
-            id (_type_, optional): _description_. Defaults to None.
         """
         super().__init__(id)
         self.width = width
