a8225fb0be1e8bfd74ae71c69d12b56bc626902a
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-02-25 16:45:46 +0300

    mm

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -97,7 +97,7 @@ class Rectangle(Base):
         for _ in range(self.y):
             print()
         for _ in range(self.height):
-            print("" * self.x + "#" * self.width)
+            print(" " * self.x + "#" * self.width)
 
     def __str__(self):
         """_summary_"""
