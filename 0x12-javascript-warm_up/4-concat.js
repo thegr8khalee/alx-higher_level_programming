4705c9fcc4abfcc985ce37df8f40a6b0234c76c1
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-03-11 21:13:44 +0300

    mm

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,2 @@
-#!/user/bin/node
-const args = process.argv.slice(2);
-const first = argv[0];
-const second = argv[1];
-console.log(first + ' is ' + fun);
+#!/usr/bin/node
+console.log(`${process.argv[2]} is ${process.argv[3]}`);
