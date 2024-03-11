0ec3e0287f370c8b828961784c897cfeaae47fcc
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-03-11 22:49:17 +0300

    mm

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
-const n = Math.floor(Number(process.argv[2]));
-for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-        console.log('X');
-    }
-    console.log('\n');
+const size = Math.floor(Number(process.argv[2]));
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let r = 0; r < size; r++) {
+    let row = '';
+    for (let c = 0; c < size; c++) row += 'X';
+    console.log(row);
+  }
 }
