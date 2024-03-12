7266deb7de845c1f65de12efca36aae848772231
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-03-12 22:43:37 +0300

    mm

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-  const count = 0;
+  let count = 0;
   for (let i = 0; i < list.length; i++) {
     if (searchElement === list[i]) {
       count++;
