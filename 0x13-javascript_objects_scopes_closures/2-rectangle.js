4f7747ee20b9c0e64a6a458e360eef29b1fe8718
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-03-12 21:48:34 +0300

    mm

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 class Rectangle {
   constructor(w, h) {
-    if (w <= 0 || h <= 0) {
-      this.width = 0;
-      this.height = 0;
-    } else {
-        this.width = w;
-        this.height = h;
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
     }
   }
 }
