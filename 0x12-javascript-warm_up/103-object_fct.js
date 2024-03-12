7f1ee9b3315c0d850ad8c70d5dee520626070cf2
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-03-12 20:11:10 +0300

    mm

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -3,14 +3,15 @@ const myObject = {
   type: 'object',
   value: 12
 };
+
 console.log(myObject);
 myObject.incr = function () {
   this.value++;
 };
+
 myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
-  
