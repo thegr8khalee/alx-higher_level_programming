567e5eca074a26f09ec639701c06300e59e8691d
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-03-12 22:22:42 +0300

    mm

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor(size) {
+    const len = process.argv.length;
+    if (len > 2) {
+        return;
+    } else {
+      super(size, size);
+    }
+  }
+}
