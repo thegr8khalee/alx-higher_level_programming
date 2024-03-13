87a837cc6276d1579eb2d9b69705343ffe419405
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-03-13 19:47:54 +0300

    mm

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,23 +1,23 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor(w, h) {
-      if ((w > 0) && (h > 0)) {
-        this.width = w;
-        this.height = h;
-      }
-    }
-    print() {
-        const size = Math.floor(Number(process.argv[2]));
-        if (!this.width || !this.height) {
-            return;
-        } else {
-          for (let r = 0; r < this.height; r++) {
-            let row = '';
-            for (let c = 0; c < this.width; c++) row += 'X';
-              console.log(row);
-          }
-        }
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
     }
   }
+  print() {
+    const size = Math.floor(Number(process.argv[2]));
+    if (!this.width || !this.height) {
+      return;
+    } else {
+      for (let r = 0; r < this.height; r++) {
+        let row = '';
+        for (let c = 0; c < this.width; c++) row += 'X';
+          console.log(row);
+      }
+    }
+  }
+}
   
-  module.exports = Rectangle;
+module.exports = Rectangle;
