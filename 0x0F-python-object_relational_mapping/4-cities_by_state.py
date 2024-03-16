b2946cc435b39f44c59cea7c23b56c853249ef4b
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-03-16 14:09:28 +0300

    mm

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 
     cu = db.cursor()
     cu.execute(
-        """SELECT states.id, states.name, cities.name
+        """SELECT cities.id, cities.name, states.name
             FROM cities
             INNER JOIN states 
             ON states.id = cities.state_id"""
