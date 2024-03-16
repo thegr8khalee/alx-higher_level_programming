4d8ca196c98157d7ec0371ae081b1ec2dacccafe
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-03-16 22:11:57 +0300

    mm

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""contains the class definition of a State and an instance Base = declarative_base()"""
+
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+mymd = MetaData()
+Base = declarative_base(metadata=mymd)
+
+
+class State(Base):
+    """inherits from Base"""
+
+    __tablename__ = "states"
+    id = id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="states")
