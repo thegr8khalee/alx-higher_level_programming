531749ab6056b2964b0b6a78c09198a1b4252781
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-03-27 15:57:41 +0300

    mm

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-    """function that finds a peak in a list of unsorted integers."""
+"""Defines a peak-finding algorithm."""
+
 
 def find_peak(list_of_integers):
-    """function that finds a peak in a list of unsorted integers."""
-    length = len(list_of_integers)
-    mid = length // 2
-    if length == 0:
+    """ Finds the peak in a list of integers """
+    if list_of_integers == []:
         return None
-    elif length == 1:
-        return list_of_integers[0]
-    else:
-        try:
-            li = list_of_integers
-            if (li[mid] >= li[mid - 1] if mid - 1 >= 0 else True) and \
-                (li[mid] >= li[mid + 1] if mid + 1 < length else True):
-                return li[mid]
-            elif li[mid - 1] > li[mid]:
-                return find_peak(li[:mid])
-            else:
-                return find_peak(li[mid + 1:])
-        except IndexError:
-            pass
-            
+
+    length = len(list_of_integers)
+    m = int(length / 2)
+    li = list_of_integers
+
+    if m - 1 < 0 and m + 1 >= length:
+        return li[m]
+    elif m - 1 < 0:
+        return li[m] if li[m] > li[m + 1] else li[m + 1]
+    elif m + 1 >= length:
+        return li[m] if li[m] > li[m - 1] else li[m - 1]
+
+    if li[m - 1] < li[m] > li[m + 1]:
+        return li[m]
+
+    if li[m + 1] > li[m - 1]:
+        return find_peak(li[m:])
+    return find_peak(li[:m])
