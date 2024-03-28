f9a09f77b373509b2c3f9783c6d77d4b8bda5d0c
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-03-28 17:47:43 +0300

    mm

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,7 +3,7 @@
 
 import requests
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     html = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(html.text)))
