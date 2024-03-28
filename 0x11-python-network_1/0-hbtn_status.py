95a2c7cd9d8d6b5332b93411e22e4e3d42f62e06
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-03-28 17:51:15 +0300

    mm

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,13 +4,9 @@
 import urllib.request
 
 if __name__ == "__main__":
-    try:
-        with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-            html = response.read()
-    except Exception as e:
-        print(e)
-
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        html = response.read()
     print("Body response:")
-    print("    - type: {}".format(type(html)))
-    print("    - content: {}".format(html))
-    print("    - utf8 content: {}".format(html.decode("utf-8")))
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode("utf-8")))
